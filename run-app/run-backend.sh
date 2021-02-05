cb0f00e9ce6ef02779463e68bf43c5e6fa00cb9d
mpgarciav <51894580+mpgarciav@users.noreply.github.com>
2021-02-05 13:20:42 -0500

    Added necessary things to run the app

--- run-app/run-backend.sh
+++ run-app/run-backend.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
 echo "Hello from backend!"
+
+echo "export PORT=4001" >>  /home/vagrant/.profile
+source  /home/vagrant/.profile
+cd /shared
+nohup ./vuego-demoapp > output.out 2>&1 &
+ps -aux
